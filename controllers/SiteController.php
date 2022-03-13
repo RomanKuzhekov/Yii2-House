9948b730056198d7e1282527441bcd8adf744332
RomanKuzhekov <profet@inbox.ru>
2022-03-13 14:36:33 +0300

    Создаем главный контроллер

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -12,31 +12,7 @@ use app\models\ContactForm;
 
 class SiteController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
+
 
     /**
      * {@inheritdoc}
