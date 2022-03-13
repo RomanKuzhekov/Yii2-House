351c50abac15425d4b9eb524bca447b52a539deb
RomanKuzhekov <profet@inbox.ru>
2022-03-13 21:58:32 +0300

    Администраторская часть сайта

---
+++ assets/AppAdminAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 13.03.2022
+ * Time: 20:36
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class AppAdminAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/reset.css',
+        'css/style.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
---
+++ web/app/modules/admin/views/guestbook/_search.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\GuestbookSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="guestbook-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <?= $form->field($model, 'text') ?>
+
+    <?= $form->field($model, 'date') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
